187de87b51e1eef801c7af492394b8efa1b26629
ruijpedro <rjpedro@gmail.com>
2025-10-07 14:50:50 +0100

    Create Color.kt

---
+++ app/src/main/java/com/rjp/smartstruct/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.rjp.smartstruct.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlueCype = Color(0xFF1E88E5)   // Azul tipo CYPE
+val GrayBG = Color(0xFFF3F5F7)     // Fundo neutro
