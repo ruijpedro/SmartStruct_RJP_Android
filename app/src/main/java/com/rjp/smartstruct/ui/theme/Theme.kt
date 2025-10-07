18872b446463e2c145c072cff7ef12b0fd5a4115
ruijpedro <rjpedro@gmail.com>
2025-10-07 14:51:43 +0100

    Create Theme.kt

---
+++ app/src/main/java/com/rjp/smartstruct/ui/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.rjp.smartstruct.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColorScheme(
+    primary = BlueCype,
+    background = GrayBG,
+    surface = Color.White,
+)
+
+@Composable
+fun SmartTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = Typography(),
+        content = content
+    )
+}
